0df814dbe2bbe7470d78470fee71f343ce71bb92
Raspberry Pi User <pi@raspberrypi.(none)>
2012-06-06 10:04:30 +0100

    corrected fusebits

--- extensionBoard/software/FUSES.sh
+++ extensionBoard/software/FUSES.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # SET FUSE TO INTERNAL 8 MHZ
-avrdude -c usbasp -p m8 -P usb -U lfuse:w:0xE4:m
+#avrdude -c usbasp -p m8 -P usb -U lfuse:w:0xE4:m
 # for external 8Mhz:
-#-U lfuse:w:0xe0:m -U hfuse:w:0xd9:m
+avrdude -c usbasp -p m8 -P usb -U lfuse:w:0xfe:m 
+avrdude -c usbasp -p m8 -P usb -U hfuse:w:0xd9:m
