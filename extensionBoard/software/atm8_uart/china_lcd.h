2a33be809d6ce8539c2f98b79285001406271cf7
Tobi <tobi@googlemail.com>
2012-10-21 16:47:04 +0200

    included 1.8 spi lcd lib

---
+++ extensionBoard/software/atm8_uart/china_lcd.h
@@ -0,0 +1,120 @@
+#include <avr/pgmspace.h>
+
+/******************** PORT AND PINS ************************************/
+/*
+ * change these according to your needs
+ *
+ */
+#define RSREG DDRD
+#define RSTREG DDRD
+#define RSPORT PORTD
+#define RSTPORT PORTD
+#define RS PD7
+#define RST PD6
+
+static int _width = 128;
+static int _height = 160;
+
+/*
+ * available functions:
+ */
+void init(void);
+void setAddrWindow(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+void fillScreen(uint16_t color);
+void drawPixel(int16_t x, int16_t y, uint16_t color);
+void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+void fillRect(int16_t x, int16_t y, int16_t w, int16_t h,uint16_t color);
+void invertDisplay(unsigned char i);
+uint16_t Color565(uint8_t r, uint8_t g, uint8_t b);
+
+
+/*
+ * do not touch the following defines if you do not know
+ * what you are doing
+ */
+#define byte uint8_t
+
+/********************** START SPI STUFF *********************************/
+#define LSBFIRST 0
+#define MSBFIRST 1
+
+#define SPI_CLOCK_DIV4 0x00                                                                                                                                                           
+#define SPI_CLOCK_DIV16 0x01
+#define SPI_CLOCK_DIV64 0x02
+#define SPI_CLOCK_DIV128 0x03
+#define SPI_CLOCK_DIV2 0x04
+#define SPI_CLOCK_DIV8 0x05
+#define SPI_CLOCK_DIV32 0x06
+
+#define SPI_MODE0 0x00
+#define SPI_MODE1 0x04
+#define SPI_MODE2 0x08
+#define SPI_MODE3 0x0C
+
+#define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
+#define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
+#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
+/*********************** END SPI STUFF **********************************/
+
+#define INITR_GREENTAB 0x0
+#define INITR_REDTAB   0x1
+
+#define ST7735_TFTWIDTH  128
+#define ST7735_TFTHEIGHT 160
+
+#define ST7735_NOP     0x00
+#define ST7735_SWRESET 0x01
+#define ST7735_RDDID   0x04
+#define ST7735_RDDST   0x09
+
+#define ST7735_SLPIN   0x10
+#define ST7735_SLPOUT  0x11
+#define ST7735_PTLON   0x12
+#define ST7735_NORON   0x13
+
+#define ST7735_INVOFF  0x20
+#define ST7735_INVON   0x21
+#define ST7735_DISPOFF 0x28
+#define ST7735_DISPON  0x29
+#define ST7735_CASET   0x2A
+#define ST7735_RASET   0x2B
+#define ST7735_RAMWR   0x2C
+#define ST7735_RAMRD   0x2E
+
+#define ST7735_PTLAR   0x30
+#define ST7735_COLMOD  0x3A
+#define ST7735_MADCTL  0x36
+
+#define ST7735_FRMCTR1 0xB1
+#define ST7735_FRMCTR2 0xB2
+#define ST7735_FRMCTR3 0xB3
+#define ST7735_INVCTR  0xB4
+#define ST7735_DISSET5 0xB6
+
+#define ST7735_PWCTR1  0xC0
+#define ST7735_PWCTR2  0xC1
+#define ST7735_PWCTR3  0xC2
+#define ST7735_PWCTR4  0xC3
+#define ST7735_PWCTR5  0xC4
+#define ST7735_VMCTR1  0xC5
+
+#define ST7735_RDID1   0xDA
+#define ST7735_RDID2   0xDB
+#define ST7735_RDID3   0xDC
+#define ST7735_RDID4   0xDD
+
+#define ST7735_PWCTR6  0xFC
+
+#define ST7735_GMCTRP1 0xE0
+#define ST7735_GMCTRN1 0xE1
+
+// Color definitions
+#define ST7735_BLACK   0x0000
+#define ST7735_BLUE    0x001F
+#define ST7735_RED     0xF800
+#define ST7735_GREEN   0x07E0
+#define ST7735_CYAN    0x07FF
+#define ST7735_MAGENTA 0xF81F
+#define ST7735_YELLOW  0xFFE0
+#define ST7735_WHITE   0xFFFF
